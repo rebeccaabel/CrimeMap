1846d1908aaa32961c9443ff377e7f0bc63953da
rebeccaabel <rebeccaabel@hotmail.se>
2023-10-18 09:24:20 +0200

    added flaskapi and ascync function in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 
-const App = () => {
-  const position = [51.505, -0.09];
+const App = () => {                 //declares a functional component named app 
+  const position = [62, 15];
+  const [markers, setMarkers] = useState ([]);
+
+  useEffect (() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/markers");
+        if(!response.ok) {
+          throw new Error("Failed to fetch data"); 
+        }
+        const data = await response.json();
+        setMarkers(data);
+      } catch (error) {
+        console.error(error)
+      }
+    };
+
+  }, []);
+
   return (
     <MapContainer
       center={position}
-      zoom={13}
+      zoom={5.4}
       scrollWheelZoom={true}
       style={{ minHeight: "100vh", minWidth: "100vw" }}
     >
@@ -16,11 +34,14 @@ const App = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+
+      {markers.map((marker, index) => (
+        <Marker key={index} position={[marker.lat, marker.lon]}>
+          <Popup>
+            {marker["Tyoe of crime"]} <br /> {marker["Location"]} <br /> {marker["Date"]}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
---
+++ server/flaskapi.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+app = Flask(__name__)
+
+URI = 'mongodb+srv://rebeccaabel:StGzyB9rUeSaVF9i@cluster0.tgvserh.mongodb.net/?retryWrites=true&w=majority'
+
+client = MongoClient(URI, server_api=ServerApi('1'))
+db = client["CrimeMap"]
+collection = db["crime_data"]
+
+collection.create_index("Date", unique=True)
+
+try: 
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+@app.route('/api/markers')
+def get_markers():
+    markers = list(collection.find({}, {"_id": 0}))
+
+    return jsonify(markers)
+
+if __name__ == '__main__':
+    app.run(debug=True)
