d3d4e93410d8423dfe273569f82ade62091a4dd0
rebeccaabel <rebeccaabel@hotmail.se>
2023-12-15 14:21:40 +0100

    deleted print(google_api_key) oops

--- geocode.py
+++ geocode.py
@@ -3,13 +3,12 @@ import googlemaps
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env
+
 load_dotenv()
 
-# Access environment variables
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
-# Sample MongoDB document
+
 document = {
     "_id": {"$oid": "657b08d85a865694b0750ced"},
     "updated_time": "14 december 14.46",
@@ -18,19 +17,17 @@ document = {
     "preamble": "Barnvagnbrand i trappuppgång",
     "text": "12:24 Polis och räddningstjänst larmades till Esplanaden i Oxelösunds centrum. En barnvagn hade av oklar anledning börjat brinna på nedre botten i en trappuppgång. Rök spred sig och boende fick in rök.\nBranden är släckt och ambulanspersonal kontroller om personer som andats in rök mår bra.\nPolisen utreder vad som inträffat.\n13:02 Polisen misstänker att branden var anlagd och öppnar därför en förundersökning rörande mordbrand.\nFem personer blev milt rökskadade varav ett barn. Platsen spärras av.\n14:10 Sju personer fördes till sjukhus för kontroll av rökskador.\n14:45 Totalt åtta personer fördes med ambulans till sjukhus för vård mot rökskador."
 }
-print("Google API Key:", google_api_key)
 
-# Initialize Google Maps API client
+
 gmaps = googlemaps.Client(key=google_api_key)
 
-# Extract locations from different fields
+# Extract locations
 location_fields = ["title_and_location", "preamble", "text"]
 locations = []
 
 for field in location_fields:
     matches = re.findall(r'\b(?:[A-ZÖÄÅa-zöäå.-]+(?:\s[A-ZÖÄÅa-zöäå.-]+)*)\b', document[field])
     for match in matches:
-        # Use Google Geocoding API to get location information
         geocode_result = gmaps.geocode(match, language='sv')
         if geocode_result:
             location_info = geocode_result[0]
@@ -39,13 +36,12 @@ for field in location_fields:
             if location:
                 locations.append({"name": match, "coordinates": (location['lat'], location['lng'])})
 
-# Define a hierarchy of location types
 location_hierarchy = ["area", "street", "city", "county"]
 
-# Sort locations based on hierarchy
+# Sort based on hierarchy
 locations.sort(key=lambda loc: next((h for h in location_hierarchy if h in loc["name"].lower()), "country"))
 
-# Determine the most specific location (last one in the sorted list)
+# Determine most specific location = last one in the sorted list
 most_specific_location = locations[-1] if locations else None
 
 print("All locations found:", locations)
